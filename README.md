a598242236e4acd778fcdea9ba66ccf600491efe
Gastón Camú <camugaston2002@gmail.com>
2024-08-31 15:35:05 -0300

    creacion entidad empleado y actualizacion readme

--- README.md
+++ README.md
@@ -14,3 +14,22 @@ Spring Initializr: https://start.spring.io/#!type=maven-project&language=java&pl
 
 - **Lombok** pero se descarto debido a la obligación de descargar un plugin en los diferentes IDE y en la posible colisión con otras dependencias así de como desconocer la implementación de los métodos.
 - **MapStruct** pero se descarto debido a las colisiones que puede tener con las otras dependencias y desconocer la implementación de los métodos.
+
+# Estructura de directorios
+
+## /controller
+
+## /dto
+
+## /entity
+
+### Empleado
+El manejo de la creacion de la fechaCreacion se hizo con PrePersist ya que se toma la fecha del momento exacto en el que se guarda el empleado en la base de datos.
+
+## /exception
+
+## /mapper
+
+## /repository
+
+## /service
---
+++ src/main/java/ApiRest/TurnosRotativos/entity/Empleado.java
@@ -0,0 +1,98 @@
+package ApiRest.TurnosRotativos.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name="empleados")
+public class Empleado {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "nro_documento", nullable = false)
+    private int nroDocumento;
+
+    @Column(name = "nombre", nullable = false)
+    private String nombre;
+
+    @Column(name = "apellido", nullable = false)
+    private String apellido;
+
+    @Column(name= "email", nullable = false)
+    private String email;
+
+    @Column(name = "fecha_nacimiento", nullable = false)
+    private LocalDate fechaNacimiento;
+
+    @Column(name = "fecha_ingreso", nullable = false)
+    private LocalDate fechaIngreso;
+
+    @Column(name = "fecha_creacion", nullable = false, updatable = false)
+    private LocalDate fechaCreacion;
+
+    @PrePersist
+    protected void onCreate() {
+        this.fechaCreacion = LocalDate.now();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getNroDocumento() {
+        return nroDocumento;
+    }
+
+    public void setNroDocumento(int nroDocumento) {
+        this.nroDocumento = nroDocumento;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(LocalDate fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public LocalDate getFechaIngreso() {
+        return fechaIngreso;
+    }
+
+    public void setFechaIngreso(LocalDate fechaIngreso) {
+        this.fechaIngreso = fechaIngreso;
+    }
+
+    public LocalDate getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+}
